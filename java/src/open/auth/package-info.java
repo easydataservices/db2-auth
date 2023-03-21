f30b6c38b5e71d8bfcd32c320544c52360e7d68f
Jeremy Rickard <jeremy.rickard@easydataservices.com>
2023-03-21 19:44:25 +1300

    Updated Javadoc

---
+++ java/src/open/auth/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides the API for persisting and retrieving sessions and attributes that are stored in an AUTH Service repository, hosted by
+ * a DB2 for LUW database.
+ *
+ * @author jeremy.rickard@easydataservices.com
+ */
+package com.easydataservices.open.auth;
---
+++ java/src/open/auth/util/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains miscellaneous auxiliary classes for the AUTH Service.
+ *
+ * @author jeremy.rickard@easydataservices.com
+ */
+package com.easydataservices.open.auth.util;
